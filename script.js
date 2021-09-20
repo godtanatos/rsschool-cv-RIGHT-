46030d72cda86aef1d1cab17b7d84bfd40c63c03
godtanatos <78075671+godtanatos@users.noreply.github.com>
2021-09-20 17:56:06 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const elBlock = document.querySelector(".description");
+const elToggle = document.querySelector(".btn")
+elToggle.addEventListener("click", () => {
+    elToggle.classList.toggle('active');
+    if (elBlock.style.height === "0px" || elBlock.style.height === "") {
+        elBlock.style.height = `${elBlock.scrollHeight}px`
+    } else {
+        elBlock.style.height = `${elBlock.scrollHeight}px`;
+        window.getComputedStyle(elBlock, null).getPropertyValue("height");
+        elBlock.style.height = "0";
+    }
+});
+elBlock.addEventListener("transitionend", () => {
+    if (elBlock.style.height !== "0px") {
+        elBlock.style.height = "auto"
+    }
+});
